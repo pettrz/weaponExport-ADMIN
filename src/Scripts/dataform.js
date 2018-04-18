e2268efb85985d8d240a6c6107eb3cb334e4aaac
Joakim Linna <joakimlinna1998@gmail.com>
2018-04-18 14:07:24 +0200

    fixed bindings

---
+++ src/Scripts/dataform.js
@@ -0,0 +1,73 @@
+var xhttp = new XMLHttpRequest();
+xhttp.open('POST', 'http://localhost:1137/add', true);
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById('confirm').innerHTML = 'Skickat';
+    }
+}
+
+Collection = function(code, name){
+    this.collectionCode = code;
+    this.collectionName = name;
+}
+
+ko.extenders.required = function(target, overrideMessage) {
+    target.hasError = ko.observable();
+    target.validationMessage = ko.observable();
+
+    function validate(newValue) {
+        target.hasError(newValue ? false : true);
+        target.validationMessage(newValue ? '' : overrideMessage || "This field is required");
+    }
+
+    validate(target());
+
+    target.subscribe(validate);
+    
+    return target;
+}
+
+var viewModel = {
+    self: this,
+    country: ko.observable('').extend({required: "Please enter a country"}),
+    code: ko.observable('').extend({required: "Please enter a code"}),
+    FHstatus: ko.observable('').extend({required: "Please enter an FH status"}),
+    gpi: ko.observable('').extend({required: "Please enter a gpi"}),
+    info: ko.observable('').extend({required: "Please enter info"}),
+    links: ko.observable('').extend({required: "Please enter one or more links"}),
+    collections: ko.observableArray([
+        new Collection('AF', 'Afrika'),
+        new Collection('AS', 'Asien'),
+        new Collection('EU', 'Europa'),
+        new Collection('NA', 'Nordamerika'),
+        new Collection('OC', 'Oceanien'),
+        new Collection('SA', 'Sydamerika'),
+    ]),
+    selectedCollection : ko.observable(''),
+    postCountry: () => {
+        //xhttp.send(prepCountry());
+        console.log(prepCountry());
+    },
+    buttonToggle: ko.observable('visible'),
+}
+
+ko.applyBindings(viewModel);
+
+var prepCountry = () => {
+    
+    return {
+        country: viewModel.country(),
+        code: viewModel.code(),
+        FHstatus: viewModel.FHstatus(),
+        info: viewModel.info(),
+        links: viewModel.links(),
+        collection: viewModel.selectedCollection().collectionCode,
+    }
+}
+
+const linkInput = <input type="text" placeholder="Länk" data-bind="value: links, valueUpdate: 'afterkeydown'"></input>;
+const linkError = <span data-bind="visible: links.hasError, text: links.validationMessage"></span>;
+const linkBox = linkInput + linkError + <br/>;
+ReactDOM.render(linkBox, document.getElementById('linkArea'));
